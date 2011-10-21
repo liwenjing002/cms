c67ec181cce95a7f18fe8a485d2221324462c2a3
unknown <test@.(none)>
2011-10-21 16:56:45 +0800

    add friend_link

--- db/migrate/20111020140532_add_code_to_forum.rb
+++ db/migrate/20111020140532_add_code_to_forum.rb
@@ -8,11 +8,12 @@ class AddCodeToForum < ActiveRecord::Migration
     Forum.create({:name=>"政策法规",:order_num=>2,:code=>"zhence"})
     Forum.create({:name=>"标准规范",:order_num=>3,:code=>"guifang"})
     Forum.create({:name=>"公报年报",:order_num=>4,:code=>"gongbao"})
-    Forum.create({:name=>"办事指南",:order_num=>5,:code=>"yenei"})
+    Forum.create({:name=>"办事指南",:order_num=>5,:code=>"huanbaozhinan"})
     Forum.create({:name=>"招投标信息",:order_num=>6,:code=>"zhaobiao"})
     Forum.create({:name=>"最新技术",:order_num=>7,:code=>"jishu"})
     Forum.create({:name=>"环境标准",:order_num=>8,:code=>"biaozhun"})
     Forum.create({:name=>"科普知识",:order_num=>9,:code=>"kepu"})
+
 
     f = Forum.find_by_code("zixun")
     Forum.create({:name=>"头条新闻",:order_num=>1,:code=>"toutiao",:father_id=>f.id})
