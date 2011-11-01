191b70d94d87ed34f2d32d856b19c58f073cc82a
unknown <test@.(none)>
2011-11-01 13:17:45 +0800

    fd

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 class HomesController < ApplicationController
    before_filter :get_head
 
--- app/models/forum.rb
+++ app/models/forum.rb
@@ -6,7 +6,7 @@ validates_uniqueness_of :code,:message => "版块代号已经被使用"
  belongs_to :father,:class_name =>"Forum"
 
   has_many :children ,:class_name=>"Forum",:foreign_key=>"father_id", :order => "updated_at desc"
-  has_many :articles_father , :class_name=>"Article",:dependent => :destroy,:foreign_key=>"first_forum_id",:conditions => "forum_id is null or forum_id =''", :order => "updated_at desc"
+  has_many :articles_father , :class_name=>"Article",:dependent => :destroy,:foreign_key=>"first_forum_id",:conditions => "forum_id is null ", :order => "updated_at desc"
   has_many :articles , :dependent => :destroy
 
 end
