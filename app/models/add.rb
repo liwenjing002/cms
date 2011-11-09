cf31c7b699f62c50d923f65391dfed7a51d62ab0
unknown <test@.(none)>
2011-11-09 15:22:44 +0800

    1

--- app/models/add.rb
+++ app/models/add.rb
@@ -1,5 +1,6 @@
+# encoding: utf-8
 class Add < ActiveRecord::Base
-  validates_presence_of :name,:message => "名称不为空"
-validates_presence_of :url,:message => "广告链接不为空"
+  validates_presence_of :name,:message => "名称不为�?
+validates_presence_of :url,:message => "广告链接不为�?
   belongs_to :picture,:class_name=>"Ckeditor::Picture"
 end
