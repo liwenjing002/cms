51d9668f8c7fdf71326c1fcf1f44eaef20289167
unknown <lee@lee-PC.(none)>
2012-03-23 11:13:25 +0800

    1

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -9,7 +9,11 @@ class AdminController < ApplicationController
       FileUtils.rm_r(Dir.glob(cache_dir+"/homes/")) rescue Errno::ENOENT
       RAILS_DEFAULT_LOGGER.info("Cache directory cache_dir/homes/ fully sweeped.")
       respond_to do |format|
+      if request.xhr?  
       format.js
+    else
+      format.html # index.html.erb
+    end
     end
       
 end
