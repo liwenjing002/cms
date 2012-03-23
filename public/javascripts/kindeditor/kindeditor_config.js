57ec32fea4267564edbdbcf95654575c2d78d17c
unknown <lee@lee-PC.(none)>
2012-03-23 10:20:24 +0800

    d

--- public/javascripts/kindeditor/kindeditor_config.js
+++ public/javascripts/kindeditor/kindeditor_config.js
@@ -1,6 +1,6 @@
         var editor;
         KindEditor.ready(function(K) {
-                editor = K.create('#kindeditor',{ uploadJson : '/kindeditor/upload',
+                editor = K.create('#kindeditor',{ width : '700px',height:'500px', uploadJson : '/kindeditor/upload',
                 fileManagerJson : '/kindeditor/images_list',
                 allowFileManager : true});
 
